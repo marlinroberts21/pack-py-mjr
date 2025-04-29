9382f0bf0430493beb5697782451d2aea620134b
Marlin Roberts <51375407+marlinroberts21@users.noreply.github.com>
2025-04-29 14:53:11 -0600

    bump 0.0.25

--- src/example_package_marlinroberts21/example.py
+++ src/example_package_marlinroberts21/example.py
@@ -1,14 +1,7 @@
 def add_one(number):
     return number + 1
 
-# TODO: #8 Add a docstring to this function
-
-'''
-DOCSTRTING
-
-'''
-
-
 # Finish the sub_one function
 def sub_one(number):
     return  number - 1
+
